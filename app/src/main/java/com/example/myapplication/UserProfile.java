5e0ae82eca7bc988ff1c302c792e81c04bd524ac
donib-irakihda <donib.irakihda@gmail.com>
2020-07-01 23:56:57 +0545

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/UserProfile.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+public class UserProfile {
+    public String Age;
+    public String Email;
+    public String Name;
+
+    public UserProfile(){
+
+    }
+
+    public UserProfile(String userAge, String userEmail, String userName) {
+        this.Age = userAge;
+        this.Email = userEmail;
+        this.Name = userName;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
